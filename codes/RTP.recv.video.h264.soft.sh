5c804d12f11c8e1d05f74a77f882c05c871181d3
Bakowski <89449454+smartcomputerlab@users.noreply.github.com>
2023-09-28 18:17:06 +0200

    Delete codes/RTP.recv.video.h264.soft.sh

--- codes/RTP.recv.video.h264.soft.sh
+++
@@ -1,3 +0,0 @@
-source config.sh
-gst-launch-1.0 -v udpsrc port=$PORT_RTP_VIDEO caps = "application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264, payload=(int)96" ! queue ! rtph264depay ! decodebin ! videoconvert ! ximagesink
-
