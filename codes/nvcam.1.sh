678a59ec06b9e3eb2fdf2b1643893d1a348fdcf6
Bakowski <89449454+smartcomputerlab@users.noreply.github.com>
2023-09-28 18:18:52 +0200

    Delete codes/nvcam.1.sh

--- codes/nvcam.1.sh
+++
@@ -1,3 +0,0 @@
-gst-launch-1.0 nvcamerasrc num-buffers=300 fpsRange="30.0 30.0" ! 'video/x-raw(memory:NVMM), width=(int)1920, height=(int)1080, format=(string)I420,framerate=(fraction)30/1' ! \
-nvvidconv flip-method=2 ! 'video/x-raw(memory:NVMM), format=(string)I420' ! nvoverlaysink -e
- 
