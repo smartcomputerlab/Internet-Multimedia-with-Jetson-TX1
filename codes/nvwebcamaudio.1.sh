b7ec419e47a86dbd3f653e47667afa0fdecb0665
Bakowski <89449454+smartcomputerlab@users.noreply.github.com>
2023-09-28 18:23:37 +0200

    Delete codes/nvwebcamaudio.1.sh

--- codes/nvwebcamaudio.1.sh
+++
@@ -1,5 +0,0 @@
-gst-launch-1.0 -e v4l2src  device="/dev/video0" ! 'video/x-raw,width=640,height=480,framerate=30/1' ! queue ! x264enc tune=zerolatency ! mux. \
-alsasrc device=hw:2 ! audio/x-raw,channels=2 ,rate=16000 ! queue ! audioconvert ! lamemp3enc ! qtmux name=mux ! \
- filesink location=samples/webcammux.mp4 sync=false
-
-
