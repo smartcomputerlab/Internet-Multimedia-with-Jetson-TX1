462fd1ef2d0893ea015670ccf04b1b0031224d42
Bakowski <89449454+smartcomputerlab@users.noreply.github.com>
2023-09-28 18:19:02 +0200

    Delete codes/RTP.send.audio.speex.sh

--- codes/RTP.send.audio.speex.sh
+++
@@ -1,3 +0,0 @@
-source config.sh
-gst-launch-1.0 -v alsasrc device=hw:2 ! audio/x-raw,rate=16000,channels=2 ! audioconvert ! speexenc ! queue ! rtpspeexpay ! udpsink host=$CLIENT port=$PORT_RTP_AUDIO
-
