89f5ef0be0e1823c77754f014a09f5fded792cb0
Bakowski <89449454+smartcomputerlab@users.noreply.github.com>
2023-09-28 18:23:18 +0200

    Delete codes/nvudpsendwebcamvideoaudio.1.sh

--- codes/nvudpsendwebcamvideoaudio.1.sh
+++
@@ -1,4 +0,0 @@
-gst-launch-1.0 -v v4l2src device="/dev/video0" ! 'video/x-raw,width=320,height=240,framerate=30/1' ! queue ! videoconvert ! jpegenc ! udpsink host=192.168.43.159 port=5005 \
- alsasrc device=hw:2 ! 'audio/x-raw,rate=16000,width=16,channels=2' ! queue ! lamemp3enc ! udpsink host=192.168.43.159 port=5006
-
-
