08f18f0381ae3c28379b700e17b9fb5b53a870ad
Bakowski <89449454+smartcomputerlab@users.noreply.github.com>
2023-09-28 18:16:40 +0200

    Delete codes/RTP.recv.audio.speex.sh

--- codes/RTP.recv.audio.speex.sh
+++
@@ -1,3 +0,0 @@
-source config.sh
-gst-launch-1.0 -v udpsrc caps="application/x-rtp,media=(string)audio,clock-rate=(int)16000, encoding-name=(string)SPEEX, encoding-params=(string)1, payload=(int)110"  port=$PORT_RTP_AUDIO ! queue ! rtpspeexdepay ! decodebin ! audioconvert ! alsasink 
-
