f40a6b120049c2ab826cfdbf61889cd615f2da8d
Bakowski <89449454+smartcomputerlab@users.noreply.github.com>
2023-09-28 18:16:50 +0200

    Delete codes/RTP.recv.video.h264.omx.sh

--- codes/RTP.recv.video.h264.omx.sh
+++
@@ -1,4 +0,0 @@
-source config.sh
-gst-launch-1.0 udpsrc port=$PORT_RTP_VIDEO caps="application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264, encoding-params=(string)1" ! queue ! rtph264depay ! h264parse ! omxh264dec ! Queue ! videoconvert ! xvimagesink
-
-
